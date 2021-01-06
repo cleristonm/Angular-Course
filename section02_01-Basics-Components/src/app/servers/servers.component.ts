7cbc402092a265f4301fecb625cf564dc3bd0a3b
Clériston Martinelo <cleriston@gmail.com>
2021-01-06 14:47:22 -0300

    Using template in component instead of a templateUrl

--- section02_01-Basics-Components/src/app/servers/servers.component.ts
+++ section02_01-Basics-Components/src/app/servers/servers.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+  <app-server></app-server>
+  <p>HI</p>
+  <app-server></app-server>
+  `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
