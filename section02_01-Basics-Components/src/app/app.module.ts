62c55cf02850166795ca14ae6a7834c1e0464a68
Clériston Martinelo <cleriston@gmail.com>
2021-01-06 14:39:01 -0300

    Created a new component

--- section02_01-Basics-Components/src/app/app.module.ts
+++ section02_01-Basics-Components/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
