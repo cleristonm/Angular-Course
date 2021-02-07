1f0168e218207043b1db34c2110c0cde73dcdf3d
Clériston Martinelo <cleriston@gmail.com>
2021-02-07 11:46:52 -0300

    158-Setting up routes

---
+++ section03_Course_Project/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HeaderComponent } from "./header/header.component";
+import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { RecipesComponent } from "./recipes/recipes.component";
+
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+
+const appRoutes: Routes = [
+
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: 'recipes', component: RecipesComponent },
+    { path: 'shopping-list', component: ShoppingListComponent },
+]
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes),
+    ],
+    exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {
+
+}
--- section03_Course_Project/src/app/app.module.ts
+++ section03_Course_Project/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -30,6 +31,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
