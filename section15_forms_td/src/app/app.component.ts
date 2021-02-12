c04989e850a5934e3dc48451e166747840c875dc
Clériston Martinelo <cleriston@gmail.com>
2021-02-11 21:12:16 -0300

    195 TD Using ngModel with Two-Way-Binding

--- section15_forms_td/src/app/app.component.ts
+++ section15_forms_td/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'pet';
+  answer = '';
 
   onSubmit(){
     console.log(this.signupForm);
