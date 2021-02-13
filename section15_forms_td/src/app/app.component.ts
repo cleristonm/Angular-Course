2f3246e7eefc4b6caa51bb755d947ed727eaa894
Clériston Martinelo <cleriston@gmail.com>
2021-02-12 21:03:34 -0300

    200 TD Reseting Forms

--- section15_forms_td/src/app/app.component.ts
+++ section15_forms_td/src/app/app.component.ts
@@ -27,9 +27,9 @@ export class AppComponent {
     this.user.email = this.signupForm.value.userData.email;
     this.user.secretQuestion = this.signupForm.value.secret;
     this.user.answer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
+    this.user.gender = this.signupForm.value.gender;    
 
-    
+    this.signupForm.reset();
   }
 
   suggestUserName() {
