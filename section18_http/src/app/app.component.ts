79189b8b7eebdeb805cda8b178391ee6239b0e14
Clériston Martinelo <cleriston@gmail.com>
2021-02-24 22:40:44 -0300

    256. GETting Data

--- section18_http/src/app/app.component.ts
+++ section18_http/src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -23,7 +25,15 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
+  }
+
+  private fetchPosts(){
+    this.http.get(
+      'https://angular-course-section-18-default-rtdb.firebaseio.com/posts.json'
+    ).subscribe( posts => {
+      console.log(posts);
+    });
   }
 
   onClearPosts() {
