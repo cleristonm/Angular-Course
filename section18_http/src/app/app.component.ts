168b49fdc3b1071b50d356fd66f228fe97265515
Clériston Martinelo <cleriston@gmail.com>
2021-02-25 10:39:56 -0300

    261. Using a Service for Http Requests

--- section18_http/src/app/app.component.ts
+++ section18_http/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -12,43 +13,23 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postService : PostsService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.postService.fetchPosts();
   }
 
   onCreatePost(postData: Post) {
-    // Send Http request
-    this.http.post< {name: string} >(
-        'https://angular-course-section-18-default-rtdb.firebaseio.com/posts.json', 
-        postData).subscribe( (responseData) => {
-          console.log(responseData);
-        });
+    this.postService.createAndStorePost(postData.title, postData.content);
+    this.postService.fetchPosts();
   }
 
   onFetchPosts() {
-    this.fetchPosts();
+    this.postService.fetchPosts();
   }
 
   private fetchPosts(){
     this.isFetching = true;
-    this.http.get< {[key: string]: Post } >(
-      'https://angular-course-section-18-default-rtdb.firebaseio.com/posts.json'
-    )
-    .pipe( map( responseData  => {
-      const postsArray : Post[] = [];
-      for (const key in responseData) {
-        if (responseData.hasOwnProperty(key)){
-          postsArray.push( { ...responseData[key], id: key });
-        }        
-      }
-      return postsArray;
-    }))
-    .subscribe( posts => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    });
   }
 
   onClearPosts() {
