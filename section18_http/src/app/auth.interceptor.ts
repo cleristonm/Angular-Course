ef95660c1b9bf4b91e2b95c744666c07dbcb6009
Clériston Martinelo <cleriston@gmail.com>
2021-02-25 16:51:03 -0300

    273. Manipulating Request Objects273. Manipulating Request Objects

--- section18_http/src/app/auth.interceptor.ts
+++ section18_http/src/app/auth.interceptor.ts
@@ -13,7 +13,13 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    console.log("The request is on its way");    
-    return next.handle(request);
+    console.log("The request is on its way");
+
+    //Clone and modify some properties
+    const modifiedRequest = request.clone ({ 
+      headers: request.headers.append('Auth', 'asdfasd'),
+      params: request.params.append('user', '212')
+    });    
+    return next.handle(modifiedRequest);
   }
 }
