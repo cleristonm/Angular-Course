81e012c40a7e2f16d32245f3f4f8bf72436a6aa4
Clériston Martinelo <cleriston@gmail.com>
2021-02-25 17:10:42 -0300

    275. Multiple Interceptors

--- section18_http/src/app/app.module.ts
+++ section18_http/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AuthInterceptor } from './auth.interceptor';
+import { LoggingInterceptor } from './logging.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,6 +13,11 @@ import { AuthInterceptor } from './auth.interceptor';
   providers: [{
     provide : HTTP_INTERCEPTORS,
     useClass : AuthInterceptor,
+    multi: true
+  },
+  {
+    provide : HTTP_INTERCEPTORS,
+    useClass : LoggingInterceptor,
     multi: true
   }],
   bootstrap: [AppComponent]
--- section18_http/src/app/logging.interceptor.ts
+++ section18_http/src/app/logging.interceptor.ts
@@ -10,16 +10,18 @@ import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
-export class AuthInterceptor implements HttpInterceptor {
+export class LoggingInterceptor implements HttpInterceptor {
 
   constructor() {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    //Clone and modify some properties
-    const modifiedRequest = request.clone ({ 
-      headers: request.headers.append('Auth', 'asdfasd'),
-      params: request.params.append('user', '212')
-    });    
-    return next.handle(modifiedRequest);
+    console.log("Outgoing request");
+    console.log(request.url);
+    return next.handle(request).pipe( tap ( event => {
+      if (event.type === HttpEventType.Response){
+        console.log("Incoming response");
+        console.log(event.body);
+      }
+    }));
   }
 }
