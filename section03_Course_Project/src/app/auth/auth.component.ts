beccb17917452bbdba7581ef6472413b43866ca4
Clériston Martinelo <cleriston@gmail.com>
2021-03-04 16:34:25 -0300

    297. Sending Login Requests

--- section03_Course_Project/src/app/auth/auth.component.ts
+++ section03_Course_Project/src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { AuthService, AuthResponseData } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -24,21 +25,27 @@ export class AuthComponent implements OnInit {
     if (!form.valid){
       return; 
     }
+
     const email = form.value.email;
     const password = form.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+
     this.isLoading = true;
     if (this.isLoginMode){
-
+      authObs = this.authService.login(email,password)
     }else {
-      this.authService.signup(email,password).subscribe( respData => {
-        console.log(respData);
-        this.isLoading = false;
-      }, 
-      errorMessage => {
-        this.error = errorMessage;
-        this.isLoading = false;
-      });
+      authObs = this.authService.signup(email,password)
     }
+
+    authObs.subscribe( respData => {
+      console.log(respData);
+      this.isLoading = false;
+    }, 
+    errorMessage => {
+      this.error = errorMessage;
+      this.isLoading = false;
+    });
 
     
     form.reset();
--- section03_Course_Project/src/app/auth/auth.service.ts
+++ section03_Course_Project/src/app/auth/auth.service.ts
@@ -4,12 +4,13 @@ import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
-interface AuthResponseData{
+export interface AuthResponseData{
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({
@@ -37,5 +38,13 @@ export class AuthService {
       return throwError(errorMesage);
     }) )
 
+  }
+
+  login(email: string, password: string){
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='+environment.myAuthKey, {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    })
   }
 }
