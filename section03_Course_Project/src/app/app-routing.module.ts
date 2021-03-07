bfe8a218daedfe2935d96abba7dc02e8327f5d72
Clériston Martinelo <cleriston@gmail.com>
2021-03-07 20:53:20 -0300

    306. Adding an Auth Guard

--- section03_Course_Project/src/app/app-routing.module.ts
+++ section03_Course_Project/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthComponent } from "./auth/auth.component";
+import { AuthGuard } from "./auth/auth.guard";
 import { HeaderComponent } from "./header/header.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
@@ -14,11 +15,14 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 const appRoutes: Routes = [
 
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes', component: RecipesComponent, children:[
-        { path: '', component: RecipeStartComponent},
-        { path: 'new', component: RecipeEditComponent },
-        { path: ':id', component: RecipeDetailComponent },        
-        { path: ':id/edit', component: RecipeEditComponent },
+    { path: 'recipes',             
+        component: RecipesComponent, 
+        canActivate: [AuthGuard],
+        children:[
+            { path: '', component: RecipeStartComponent},
+            { path: 'new', component: RecipeEditComponent },
+            { path: ':id', component: RecipeDetailComponent },        
+            { path: ':id/edit', component: RecipeEditComponent },
         
     ], resolve: [RecipesResolverService] },
     { path: 'shopping-list', component: ShoppingListComponent },
