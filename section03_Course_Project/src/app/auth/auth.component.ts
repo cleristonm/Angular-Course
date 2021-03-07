9d77fb6d0e821418a243dec247781c2b138e4905
Clériston Martinelo <cleriston@gmail.com>
2021-03-07 14:51:03 -0300

    300. Reflecting the Auth State in the UI

--- section03_Course_Project/src/app/auth/auth.component.ts
+++ section03_Course_Project/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService, AuthResponseData } from './auth.service';
 
@@ -12,7 +13,8 @@ export class AuthComponent implements OnInit {
   isLoading = false;
   error : string = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+      private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -41,6 +43,7 @@ export class AuthComponent implements OnInit {
     authObs.subscribe( respData => {
       console.log(respData);
       this.isLoading = false;
+      this.router.navigate(['/recipes']);
     }, 
     errorMessage => {
       this.error = errorMessage;
--- section03_Course_Project/src/app/header/header.component.ts
+++ section03_Course_Project/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core'
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core'
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 
@@ -7,11 +9,29 @@ import { DataStorageService } from '../shared/data-storage.service';
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent{
+export class HeaderComponent implements OnInit, OnDestroy{
+    isAuthenticated = false;
+    private userSub: Subscription;
+
     
     @Output() menuChanged = new EventEmitter<string>();
 
-    constructor(private dataStorageService : DataStorageService){}
+    constructor(private dataStorageService : DataStorageService,
+        private authService: AuthService){}
+
+    ngOnInit(){
+        this.userSub = this.authService.user.subscribe(
+            user => {
+                this.isAuthenticated = !!user;
+                console.log(!user);
+                console.log(!!user);
+            }
+        );
+    }
+
+    ngOnDestroy(){
+        this.userSub.unsubscribe();
+    }
 
     onSaveData(){
         this.dataStorageService.storeRecipes();        
