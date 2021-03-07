6687d872e8cb7e894429015ffb410d219fea618d
Clériston Martinelo <cleriston@gmail.com>
2021-03-07 19:35:37 -0300

    304. Adding Auto-Login

--- section03_Course_Project/src/app/auth/auth.service.ts
+++ section03_Course_Project/src/app/auth/auth.service.ts
@@ -55,6 +55,28 @@ export class AuthService {
           expirationDate
       );
       this.user.next(user);
+      localStorage.setItem('userData', JSON.stringify(user));
+  }
+  
+  autoLogin() {
+    const userData : {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData){
+      return;
+    }
+    const loadedUser = new User(
+      userData.email, 
+      userData.id, 
+      userData._token, 
+      new Date(userData._tokenExpirationDate));
+
+    if (loadedUser.token){
+      this.user.next(loadedUser);
+    }
   }
 
   logout(){
