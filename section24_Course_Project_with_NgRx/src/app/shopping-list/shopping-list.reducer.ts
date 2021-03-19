565240d503bbdd63443b2b2f69873ce06e9d7e30
Clériston Martinelo <cleriston@gmail.com>
2021-03-19 11:48:45 -0300

    347. Getting Started with Reducers

---
+++ section24_Course_Project_with_NgRx/src/app/shopping-list/shopping-list.reducer.ts
@@ -0,0 +1,12 @@
+import { Ingredient } from "../shared/ingredient.model";
+
+const inititalState = {
+    ingredients: Ingredient[] = [
+        new Ingredient('Potato', 5),
+        new Ingredient('Cheese', 1),
+      ];
+};
+
+export function shoppingListReducer(state, action){
+    
+}
