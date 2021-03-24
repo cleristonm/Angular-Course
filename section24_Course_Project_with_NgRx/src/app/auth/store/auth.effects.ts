d6ce02e65387d1958cb819f2520629e41fdc28a3
Clériston Martinelo <cleriston@gmail.com>
2021-03-24 11:40:29 -0300

    367. Effects & Error Handling

--- section24_Course_Project_with_NgRx/src/app/auth/store/auth.effects.ts
+++ section24_Course_Project_with_NgRx/src/app/auth/store/auth.effects.ts
@@ -1,10 +1,52 @@
-import { Actions, ofType } from '@ngrx/effects';
+import { HttpClient } from '@angular/common/http';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import * as AuthActions from './auth.actions';
 
+export interface AuthResponseData{
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    localId: string;
+    expiresIn: string;
+    registered?: boolean;
+  }
+
+  
 export class AuthEffecs {
+
+    @Effect()
     authLogin = this.actions$.pipe(
-        ofType(AuthActions.LOGIN_START)
+        ofType(AuthActions.LOGIN_START),
+        switchMap((authData: AuthActions.LoginStart) => {
+            return this.http.
+                post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='+environment.myAuthKey, 
+                {
+                    email: authData.payload.email,
+                    password: authData.payload.password,
+                    returnSecureToken: true
+                }
+            ).pipe( 
+                catchError( error => {
+
+                    ///To create a new observable
+                    of();
+                }), 
+                map( resData => {
+                    ///To create a new observable
+                    of();
+                })
+            );
+        }),
+        
+        
     );
 
-    constructor(private actions$: Actions){}
+    constructor(
+        private actions$: Actions,
+        private http : HttpClient
+        ){}
 }
