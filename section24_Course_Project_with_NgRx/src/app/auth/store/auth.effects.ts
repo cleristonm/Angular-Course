3ee917bd59637008ead7dc708cd44738be8636c8
Clériston Martinelo <cleriston@gmail.com>
2021-03-24 11:24:28 -0300

    366. Defining the First Effect

--- section24_Course_Project_with_NgRx/src/app/auth/store/auth.effects.ts
+++ section24_Course_Project_with_NgRx/src/app/auth/store/auth.effects.ts
@@ -3,7 +3,7 @@ import * as AuthActions from './auth.actions';
 
 export class AuthEffecs {
     authLogin = this.actions$.pipe(
-        ofType()
+        ofType(AuthActions.LOGIN_START)
     );
 
     constructor(private actions$: Actions){}
